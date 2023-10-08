cfe43bc05821b259ef8479e6a991b8196a27d6d2
nansheng.lan <nansheng.lan@zb.com>
2023-10-08 17:38:56 +0800

    feat: 页面分块布局，导入echarts组件

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,42 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div
+    class="bg-[url('assets/images/bg5.jpg')] bg-cover bg-center h-screen text-white p-5 flex overflow-hidden"
+  >
+    <div class="flex-1 mr-5 bg-opacity-50 bg-slate-800 p-3 flex flex-col">
+      <!-- 横向柱状图 -->
+      <HorizontalBar></HorizontalBar>
+      <!-- 雷达图 -->
+      <RadarBar></RadarBar>
+      <!-- 关系图 -->
+      <Relation></Relation>
+    </div>
+    <div class="w-1/2 mr-5 flex flex-col">
+      <!-- 数据总览图 -->
+      <TotalData></TotalData>
+      <!-- 地图可视化 -->
+      <MapChart></MapChart>
+    </div>
+    <div class="flex-1 bg-opacity-50 bg-slate-800 p-3 flex flex-col">
+      <!-- 竖向柱状图 -->
+      <VerticalBar></VerticalBar>
+      <!-- 环形图 -->
+      <RingBar></RingBar>
+      <!-- 文档云图 -->
+      <WordCloud></WordCloud>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<script setup>
+import HorizontalBar from "./components/HorizontalBar.vue";
+import MapChart from "./components/MapChart.vue";
+import RadarBar from "./components/RadarBar.vue";
+import Relation from "./components/Relation.vue";
+import RingBar from "./components/RingBar.vue";
+import TotalData from "./components/TotalData.vue";
+import VerticalBar from "./components/VerticalBar.vue";
+import WordCloud from "./components/WordCloud.vue";
+</script>
+
+<style>
 </style>
---
+++ src/components/TotalData.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>数据总览图</div>
+</template>
+
+<script setup></script>
+
+<style>
+
+</style>
